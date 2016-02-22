8d8291ef3cdbfa0b7c87610d8c63b078839033cf
505515 <505515@users.noreply.github.com>
2016-02-23 00:34:33 +0100

    ass2.2 and 2.3

---
+++ ProgrammingForEverybody/Week4/assignment2_2.py
@@ -0,0 +1,4 @@
+# asg 2.2
+# 2.7 input = raw_input
+name = input('Enter your name')
+print('Hello ' + name)
---
+++ ProgrammingForEverybody/Week4/assignment2_3.py
@@ -0,0 +1,8 @@
+# asg 2.3
+
+hours = float(input('How many hours? '))
+rate = float(input('What is the rate? '))
+
+pay = hours * rate
+
+print(pay)
