948e5abec669520e34bc3702b435729d8fc9de31
505515 <505515@users.noreply.github.com>
2016-03-07 20:29:37 +0100

    ass8_5

---
+++ Python Data Structures/Week4/assignment8_5.py
@@ -0,0 +1,21 @@
+file = 'mbox-short.txt'
+
+try:
+    ofile = open(file, 'r')
+except:
+    print('Could not open file:', file)
+
+
+c = 0
+
+for s in ofile:
+    s.strip()
+    if s.startswith('From '):
+        c = c + 1
+        line = s.split()
+        email = line[1]
+        print(email)
+
+print("There were", c, "lines in the file with From as the first word")
+# python27
+# print("There were % s lines in the file with From as the first word") % c
