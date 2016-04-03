51dd8e572535d7992a78fd7e6e84fef9bb15cf23
505515 <505515@users.noreply.github.com>
2016-04-03 17:26:59 +0200

    ass_13_1

---
+++ UsingPythontoAccessWebData/Week5/assignment13_1.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as ET
+import urllib.request
+
+#url = 'http://python-data.dr-chuck.net/comments_42.xml'
+url = 'http://python-data.dr-chuck.net/comments_248481.xml'
+
+try:
+    print('Retriving url:', url)
+    html = urllib.request.urlopen(url)
+    meta = html.info() # header
+    html = html.read()
+    print('Retrived', meta['Content-Length'], 'characters')
+except:
+    print('Not a valid url or could not open url', url)
+    quit()
+
+xmldata = ET.fromstring(html)
+comment = xmldata.findall('comments/comment')
+
+c = 0
+
+for i in comment:
+    x = i.find('count').text
+    c = c + int(x)
+
+print('Sum:', c)
