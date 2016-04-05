b7bd1625b63a6c894d6fdd170ebe562cf4756ad4
505515 <505515@users.noreply.github.com>
2016-04-05 07:58:19 +0200

    yr test

---
+++ UsingPythontoAccessWebData/yr.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as ET
+import urllib.request
+
+#url = 'http://www.yr.no/sted/Norge/oslo/oslo/oslo/varsel.xml'
+
+try:
+    print('Retriving url:', url)
+    html = urllib.request.urlopen(url)
+    meta = html.info() # header
+    html = html.read()
+    print('Retrived', meta['Content-Length'], 'characters')
+except:
+    print('Not a valid url or could not open url', url)
+    quit()
+
+xmldata = ET.fromstring(html)
+forecast = xmldata.findall('forecast/text/location/time')
+
+for i in forecast:
+    x = i.find('body').text
+    print(x)
